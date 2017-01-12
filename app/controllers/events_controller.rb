1de97bb7d9fa13122ce669a41052735088774340
Alex Wasson <wasson.alex@gmail.com>
2017-01-12 17:02:43 -0500

    put in modal support, event edit and delete buttons

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,26 @@ class EventsController < ApplicationController
 	def index
 		@events = current_user.events
 	end
+
+	def update
+
+		@event = Event.find params[:id]
+
+		if @event.update(event_params)
+			redirect_to :back, notice: "Date Saved!"
+
+		else
+			@alert =  "Date could not be saved! #{@event.errors.full_messages}"
+			redirect_to :back
+		end		
+	end
+
+	def destroy
+		@event = Event.find params[:id]
+		@event.delete
+		redirect_to :back, notice: "Event deleted"
+	end
+
 	private
 		def event_params
 			params.require(:event).permit! #(:title,:body,:start,:end,:day,:urgency,:location,:user_id)
